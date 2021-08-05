7fd07e1ae0e76fea24c6e5f9ecde032e82783665
iamalok12 <ialok@outlook.com>
2021-08-05 22:21:45 +0530

    screen util added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+class LoginScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200.h,
+      width: 50.w,
+      child: Text("Alok",style: TextStyle(fontSize: 50.sp),),
+    );
+  }
+}
