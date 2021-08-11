aca57105e229d87aa845e774e89edee21d8ad95f
iamalok12 <ialok@outlook.com>
2021-08-11 18:14:05 +0530

    address and name filed added

--- lib/widgets/text_widget/custom_mobile_text_field.dart
+++ lib/widgets/text_widget/custom_mobile_text_field.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:eshop/utils/utils.dart';
 
-class CustomTextWidget extends StatelessWidget {
-  final bool isObsecure;
+class CustomMobileTextField extends StatelessWidget {
   final TextEditingController controller;
-  final String Function(String) validator;
-  final String title;
-  final TextInputType keyboardType;
-  const CustomTextWidget({
+  const CustomMobileTextField({
     Key key,
-    this.title,
-    this.isObsecure = false,
-    this.keyboardType,
     this.controller,
-    this.validator,
   }) : super(key: key);
 
   @override
@@ -25,20 +17,20 @@ class CustomTextWidget extends StatelessWidget {
           ),
           color: Colors.white),
       width: 350.w,
-      // height: 60.h,
+      height: 60.h,
       margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-      // padding: const EdgeInsets.fromLTRB(10, 15, 10, 12),
+      padding: const EdgeInsets.fromLTRB(10, 15, 10, 12),
       child: Theme(
         data: Theme.of(context).copyWith(splashColor: Colors.transparent),
         child: TextFormField(
-          keyboardType: keyboardType,
-          obscureText: isObsecure,
+          keyboardType: TextInputType.number,
           style: const TextStyle(fontSize: 15.0, color: Colors.black),
           decoration: InputDecoration(
+            errorStyle: const TextStyle(height: 0),
             filled: true,
             border: InputBorder.none,
             fillColor: Colors.white,
-            hintText: title,
+            hintText: "Mobile Number",
             contentPadding: const EdgeInsets.only(left: 14.0, bottom: 12.0),
             focusedBorder: OutlineInputBorder(
               borderSide: const BorderSide(color: Colors.white),
@@ -50,7 +42,23 @@ class CustomTextWidget extends StatelessWidget {
             ),
           ),
           controller: controller,
-          validator: validator
+          validator: (text){
+            if(isNumeric(text)&&text.length==10){
+              return null;
+            }
+            else{
+              Fluttertoast.showToast(
+                  msg: "Invalid mobile number",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 2,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0
+              );
+              return "";
+            }
+          }
         ),
       ),
     );
