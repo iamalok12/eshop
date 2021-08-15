25416d3cf7efbaff2c3235e82caee5bdbb87918e
iamalok12 <ialok@outlook.com>
2021-08-15 12:23:11 +0530

    term and condition added

---
+++ lib/presentation/screens/seller_home/seller_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SellerHome extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Text("Seller"),
+      ),
+    );
+  }
+}
