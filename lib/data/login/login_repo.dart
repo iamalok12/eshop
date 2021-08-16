0096dcde980e91c23c998abf59e0b0a86d024f41
iamalok12 <ialok@outlook.com>
2021-08-16 11:11:56 +0530

    lint optimised

--- lib/data/login/login_repo.dart
+++ lib/data/login/login_repo.dart
@@ -1,28 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginRepo{
+class LoginRepo {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-    Future<void> sendOtp(
-        String phoneNumber,
-        PhoneVerificationFailed phoneVerificationFailed,
-        PhoneVerificationCompleted phoneVerificationCompleted,
-        PhoneCodeSent phoneCodeSent,
-        PhoneCodeAutoRetrievalTimeout autoRetrievalTimeout) async {
-      print("otp fn triggered");
-      print(phoneNumber);
-      await auth.verifyPhoneNumber(
-          phoneNumber: phoneNumber,
-          verificationCompleted: phoneVerificationCompleted,
-          codeSent:phoneCodeSent,
-          verificationFailed: phoneVerificationFailed,
-          codeAutoRetrievalTimeout: autoRetrievalTimeout
-      );
-    }
+  Future<void> sendOtp(
+    String phoneNumber,
+    PhoneVerificationFailed phoneVerificationFailed,
+    PhoneVerificationCompleted phoneVerificationCompleted,
+    PhoneCodeSent phoneCodeSent,
+    PhoneCodeAutoRetrievalTimeout autoRetrievalTimeout,
+  ) async {
+    print("otp fn triggered");
+    print(phoneNumber);
+    await auth.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      verificationCompleted: phoneVerificationCompleted,
+      codeSent: phoneCodeSent,
+      verificationFailed: phoneVerificationFailed,
+      codeAutoRetrievalTimeout: autoRetrievalTimeout,
+    );
+  }
+
   Future<User> verifyAndLogin(
-      String verificationId, String smsCode) async {
-    final PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: smsCode);
-    await auth.signInWithCredential(credential);
+    String verificationId,
+    String smsCode,
+  ) async {
+    final PhoneAuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: verificationId,
+      smsCode: smsCode,
+    );
+    await auth.signInWithCredential(
+      credential,
+    );
     return auth.currentUser;
   }
 }
