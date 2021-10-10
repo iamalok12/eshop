78541909375fb1745756e3d840d76cee5922cd00
iamalok12 <ialok@outlook.com>
2021-10-10 15:26:27 +0530

    sign in method changed

---
+++ lib/screens/authentication/seller_register2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SellerRegister2 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
